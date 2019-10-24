6053e4654ea61aad34217b9c488f1441093fb05e
arkoscr <tutordejava@gmail.com>
2019-10-24 11:31:10 -0600

    Webservices

---
+++ BD/visitantes.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 24-10-2019 a las 17:28:52
+-- Versión del servidor: 5.7.21
+-- Versión de PHP: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `prueba_qr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `visitantes`
+--
+
+DROP TABLE IF EXISTS `visitantes`;
+CREATE TABLE IF NOT EXISTS `visitantes` (
+  `usuario` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `recurso` varchar(56) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `visitantes`
+--
+
+INSERT INTO `visitantes` (`usuario`, `recurso`) VALUES
+('', ''),
+('Oscar', 'Cuenticos'),
+('Luis', 'Cuenticos');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
